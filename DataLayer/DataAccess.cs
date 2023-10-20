3abd07681496508a30821c2e6b320e64daa30acc
rhyth0709 <rodriguesrhyth@gmail.com>
2023-10-20 11:14:35 +0000

    Added Day 1

---
+++ DataLayer/DataAccess.cs
@@ -0,0 +1,20 @@
+﻿using Entity;
+namespace DataLayer;
+public class DataAccess
+{
+
+
+    public static List<Movie> movies = new List<Movie>()
+    {
+    new Movie{Id =1,Name="Toofan",Ryear=2021,Ratings=3},
+    new Movie{Id =2,Name="Ludo",Ryear=2020,Ratings=2},
+    new Movie{Id =3,Name="Gunjan",Ryear=2020,Ratings=3},
+    new Movie{Id =4,Name="Big Bull",Ryear=2021,Ratings=1},
+    new Movie{Id =5,Name="Laxmi",Ryear=2020,Ratings=3},
+    };
+
+    public List<Movie>GetMovies()
+    {
+        return movies;
+    }
+}
---
+++ Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Entity;
+using BzLayer;
+
+BiznezzLogic bz = new BiznezzLogic();
+
+List<Movie> movies = bz.GetMovies();
+
+if(movies!=null)
+{
+     foreach(var m in movies)
+     {
+        Console.WriteLine($"{m.Id} {m.Name} {m.Ratings} {m.Ryear}");
+     }
+}
+else{
+    Console.WriteLine("No Movies Present");
+}
+
